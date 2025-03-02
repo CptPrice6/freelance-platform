bb7f9f5ff0195e924782840c3df851df90acea17
Domantas <domasp902@gmail.com>
2025-03-02 20:26:10 +0200

    added app.js with react router, wrapped it in layout of header, body, footer

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import UserDashboard from "./pages/UserDashboard";
+import PrivateRoute from "./components/PrivateRoute";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        {/* Wrap all routes with the Layout component */}
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/user/dashboard"
+              element={<PrivateRoute element={<UserDashboard />} />}
+            />
+          </Routes>
+        </Layout>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
