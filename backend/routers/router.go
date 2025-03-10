45f23dfe42b9f889cb6bdfe6b9b4259b58a3d4b7
Domantas <domasp902@gmail.com>
2025-03-10 19:15:02 +0200

    added routes for clients,freelancers,skills etc

--- backend/routers/router.go
+++ backend/routers/router.go
@@ -8,21 +8,35 @@ import (
 )
 
 func init() {
-
 	web.Router("/register", &controllers.AuthController{}, "post:RegisterHandler")
 	web.Router("/login", &controllers.AuthController{}, "post:LoginHandler")
 	web.Router("/refresh", &controllers.AuthController{}, "post:RefreshTokenHandler")
 
+	// user logic
 	web.InsertFilter("/user/*", web.BeforeRouter, middleware.UserAuthMiddleware)
-
-	web.Router("/user", &controllers.UserController{})
-
+	web.Router("/user", &controllers.UserController{}, "get:GetUserHandler")
+	web.Router("/user", &controllers.UserController{}, "put:UpdateUserHandler")
+	web.Router("/user", &controllers.UserController{}, "delete:DeleteUserHandler")
 	web.Router("/user/logout", &controllers.AuthController{}, "post:LogoutHandler")
 	web.Router("/user/auth", &controllers.AuthController{}, "get:AuthHandler")
+
+	web.Router("/user/freelancer", &controllers.FreelancerController{}, "put:UpdateFreelancerDataHandler")
+	web.Router("/user/freelancer/skills", &controllers.SkillController{}, "post:AddFreelancerSkillHandler")
+	web.Router("/user/freelancer/skills", &controllers.SkillController{}, "delete:DeleteFreelancerSkillHandler")
+
+	web.InsertFilter("/skills/*", web.BeforeRouter, middleware.UserAuthMiddleware)
+	web.Router("/skills", &controllers.SkillController{}, "get:GetSkillsHandler")
+
+	web.Router("/user/client", &controllers.ClientController{}, "put:UpdateClientDataHandler")
 
 	web.InsertFilter("/admin/*", web.BeforeRouter, middleware.AdminAuthMiddleware)
 	web.Router("/admin/users", &controllers.AdminController{}, "get:GetUsersHandler")
 	web.Router("/admin/users/:id", &controllers.AdminController{}, "delete:DeleteUserHandler")
 	web.Router("/admin/users/:id", &controllers.AdminController{}, "put:UpdateUserHandler")
+
+	//TODO:
+	web.Router("/admin/skills", &controllers.SkillController{}, "post:AddSkillHandler")
+	web.Router("/admin/skills", &controllers.SkillController{}, "delete:DeleteSkillHandler")
+	web.Router("/admin/skills", &controllers.SkillController{}, "put:UpdateSkillHandler")
 
 }
