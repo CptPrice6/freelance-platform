0dec457f6bbc685f4c47e8639a5880b22e9ed552
Domantas <domasp902@gmail.com>
2025-03-10 19:16:57 +0200

    added skill model with functions to get all skills, get one skill by id

--- backend/models/skill.go
+++ backend/models/skill.go
@@ -3,14 +3,31 @@ package models
 import "github.com/beego/beego/v2/client/orm"
 
 type Skill struct {
-	Id        int    `orm:"pk;auto"`
-	SkillName string `orm:"size(50)"`
+	Id   int    `orm:"pk;auto"`
+	Name string `orm:"unique"`
 }
 
 func init() {
 	orm.RegisterModel(new(Skill))
 }
 
-func (u *Skill) TableName() string {
+func (s *Skill) TableName() string {
 	return "skills"
+}
+
+func GetAllSkills() ([]Skill, error) {
+	o := orm.NewOrm()
+	var skills []Skill
+	_, err := o.QueryTable(new(Skill)).All(&skills)
+	return skills, err
+}
+
+func GetSkillById(skillID int) (*Skill, error) {
+	o := orm.NewOrm()
+	skill := Skill{Id: skillID}
+	err := o.Read(&skill, "Id")
+	if err != nil {
+		return nil, err
+	}
+	return &skill, nil
 }
