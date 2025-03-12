40008329d215351b850e10b8d2145afb90fc9f61
Domantas <domasp902@gmail.com>
2025-03-12 13:54:37 +0200

    Added response types to be reused and for better modularity

--- backend/types/types.go
+++ backend/types/types.go
@@ -1,5 +1,7 @@
 package types
 
+import "time"
+
 // request structure for register
 type RegisterRequest struct {
 	Name     string `json:"name"`
@@ -52,4 +54,59 @@ type AddDeleteSkillRequest struct {
 
 type AddUpdateSkillRequest struct {
 	SkillName string `json:"skill_name"`
+}
+
+type Skill struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type FreelancerData struct {
+	Title        string  `json:"title"`
+	Description  string  `json:"description"`
+	Skills       []Skill `json:"skills"`
+	HourlyRate   float64 `json:"hourly_rate"`
+	WorkType     string  `json:"work_type"`
+	HoursPerWeek int     `json:"hours_per_week"`
+}
+
+type ClientData struct {
+	Description string `json:"description"`
+	CompanyName string `json:"company_name"`
+	Industry    string `json:"industry"`
+	Location    string `json:"location"`
+}
+
+type UserResponse struct {
+	ID             int             `json:"id"`
+	Email          string          `json:"email"`
+	Role           string          `json:"role"`
+	Name           string          `json:"name"`
+	Surname        string          `json:"surname"`
+	FreelancerData *FreelancerData `json:"freelancer_data,omitempty"`
+	ClientData     *ClientData     `json:"client_data,omitempty"`
+}
+
+type UserResponseForAdmins struct {
+	ID             int             `json:"id"`
+	Email          string          `json:"email"`
+	Role           string          `json:"role"`
+	Name           string          `json:"name"`
+	Surname        string          `json:"surname"`
+	Ban            bool            `json:"ban"`
+	CreatedAt      time.Time       `json:"created_at"`
+	FreelancerData *FreelancerData `json:"freelancer_data,omitempty"`
+	ClientData     *ClientData     `json:"client_data,omitempty"`
+}
+
+type FreelancerInfo struct {
+	ID         int     `json:"id"`
+	Name       string  `json:"name"`
+	Surname    string  `json:"surname"`
+	Title      string  `json:"title"`
+	HourlyRate float64 `json:"hourly_rate"`
+}
+
+type GetFreelancersResponse struct {
+	Freelancers []FreelancerInfo `json:"freelancers"`
 }
