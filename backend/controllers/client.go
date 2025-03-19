9e914662b97b1af319253cc563f59c29b8d399dc
Domantas <domasp902@gmail.com>
2025-03-19 12:08:21 +0200

    wrote functionality in client controller to get clients and client

--- backend/controllers/client.go
+++ backend/controllers/client.go
@@ -2,8 +2,10 @@ package controllers
 
 import (
 	"backend/models"
+	"backend/types"
 	"backend/validators"
 	"net/http"
+	"strconv"
 
 	"github.com/beego/beego/v2/server/web"
 )
@@ -52,4 +54,81 @@ func (c *ClientController) UpdateClientDataHandler() {
 	c.Ctx.Output.SetStatus(http.StatusOK)
 	c.Data["json"] = map[string]string{"message": "Client data updated successfully"}
 	c.ServeJSON()
+}
+
+func (c *ClientController) GetClientsHandler() {
+	users, err := models.GetUsersByRole("client")
+	if err != nil {
+		c.Ctx.Output.SetStatus(http.StatusInternalServerError)
+		c.Ctx.Output.JSON(map[string]string{"error": "Error fetching clients"}, false, false)
+		return
+	}
+
+	var clients []types.ClientInfo
+
+	for _, user := range users {
+		clientData, err := models.GetClientDataByUserID(user.Id)
+		if err != nil || clientData == nil {
+			continue
+		}
+
+		clientInfo := types.ClientInfo{
+			ID:          user.Id,
+			Name:        user.Name,
+			Surname:     user.Surname,
+			CompanyName: clientData.CompanyName,
+		}
+
+		clients = append(clients, clientInfo)
+	}
+
+	c.Ctx.Output.SetStatus(http.StatusOK)
+	c.Data["json"] = clients
+	c.ServeJSON()
+}
+
+func (c *ClientController) GetClientHandler() {
+	idStr := c.Ctx.Input.Param(":id")
+	userID, err := strconv.Atoi(idStr)
+	if err != nil {
+		c.Ctx.Output.SetStatus(http.StatusBadRequest)
+		c.Ctx.Output.JSON(map[string]string{"error": "Invalid user ID"}, false, false)
+		return
+	}
+
+	user, err := models.GetUserById(userID)
+	if user == nil || err != nil {
+		c.Ctx.Output.SetStatus(http.StatusBadRequest)
+		c.Ctx.Output.JSON(map[string]string{"error": "User not found"}, false, false)
+		return
+	}
+	if user.Role != "client" {
+		c.Ctx.Output.SetStatus(http.StatusBadRequest)
+		c.Ctx.Output.JSON(map[string]string{"error": "Client not found"}, false, false)
+		return
+	}
+
+	response := types.UserResponse{
+		ID:      user.Id,
+		Email:   user.Email,
+		Role:    user.Role,
+		Name:    user.Name,
+		Surname: user.Surname,
+	}
+
+	clientData, err := models.GetClientDataByUserID(user.Id)
+	if err == nil && clientData != nil {
+
+		response.ClientData = &types.ClientData{
+			CompanyName: clientData.CompanyName,
+			Industry:    clientData.Industry,
+			Location:    clientData.Location,
+			Description: clientData.Description,
+		}
+	}
+
+	c.Ctx.Output.SetStatus(http.StatusOK)
+	c.Data["json"] = response
+	c.ServeJSON()
+
 }
