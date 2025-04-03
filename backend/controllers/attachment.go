930355dcb3d7828e1b507b6261e86c3f8aca139a
Domantas <domasp902@gmail.com>
2025-04-03 20:55:11 +0300

    skeleton function to download attachments

---
+++ backend/controllers/attachment.go
@@ -0,0 +1,13 @@
+package controllers
+
+import (
+	"github.com/beego/beego/v2/server/web"
+)
+
+type AttachmentController struct {
+	web.Controller
+}
+
+func (c *AttachmentController) DownloadAttachment() {
+
+}
